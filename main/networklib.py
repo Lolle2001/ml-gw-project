0df7a7992fe5bbfa29861c161e75733cb9a1cb23
Lieuwe Huisman <153300723+Lolle2001@users.noreply.github.com>
2024-12-11 16:44:11 +0100

    Update

--- main/networklib.py
+++ main/networklib.py
@@ -27,12 +27,11 @@ class GlitchClassifierDynamic(nn.Module):
             nn.Linear(input_dim, 10),
             nn.ReLU(),
             nn.Linear(10, 8),
-            nn.ELU(),
+            nn.ReLU(),
             nn.Linear(8, 10),
             nn.ReLU(),
             nn.Linear(10, output_dim),
-            nn.Softmax(dim=1),
-            nn.Linear(output_dim, output_dim)
+            nn.Softmax(dim=1)
             # nn.Softmax(dim=1)  # Softmax for multi-class classification
         )
 
@@ -57,8 +56,8 @@ def train_model(model : nn.Module,
     model = model.to(device)
     loss_list=[]
     val_loss_list=[]
-    accuracy_0_list=[]
-    accuracy_1_list=[]
+    precision_list=[]
+    recall_list=[]
     accuracy_list=[]
     train_loss_list = []
     for epoch in range(epochs):
@@ -82,11 +81,13 @@ def train_model(model : nn.Module,
         # Validation phase
         model.eval()
         val_loss = 0.0
-        correct_0 = 0
-        correct_1 = 0
-        total_0 = 0
-        total_1 = 0
-        
+        # accuracy = 0
+        correct_tp = 0
+        correct_fp = 0
+        correct_fn = 0
+        correct_tn = 0
+        precision = 0
+        recall = 0
 
         with torch.no_grad():
             for batch in val_loader:
@@ -98,33 +99,51 @@ def train_model(model : nn.Module,
                 val_loss += loss.item()
 
                 _, predicted = torch.max(outputs, 1)
-                mask_0 = labels == 0
-                mask_1 = labels == 1
-                correct_0 += (predicted[mask_0] == labels[mask_0]).sum().item()
-                correct_1 += (predicted[mask_1] == labels[mask_1]).sum().item()
-                total_0 += labels[mask_0].size(0)
-                total_1 += labels[mask_1].size(0)
+                mask_actual_0 = labels == 0
+                mask_actual_1 = labels == 1
+                mask_predicted_0 = predicted == 0
+                mask_predicted_1 = predicted == 1
+                correct_tp += (predicted[mask_predicted_0] == labels[mask_predicted_0]).sum().item() # True positive
+                correct_fp += (predicted[mask_predicted_0] != labels[mask_predicted_0]).sum().item() # False positive
+                correct_fn += (predicted[mask_predicted_1] != labels[mask_predicted_1]).sum().item() # False negative
+                correct_tn += (predicted[mask_predicted_1] == labels[mask_predicted_1]).sum().item() # True negative
+                
+              
 
                 # correct += (predicted == labels).sum().item()
                 # total += labels.size(0)
 
         train_loss /= len(train_loader)
         val_loss /= len(val_loader)
-        accuracy_0 = correct_0 / total_0
-        accuracy_1 = correct_1 / total_1
-        accuracy = (correct_0 + correct_1) / (total_0+total_1)
+
+        total_positive = correct_tp + correct_fp
+        total_negative = correct_tn + correct_fn
+        if total_positive > 0:
+            precision = correct_tp/total_positive
+        total_tpfn = correct_tp + correct_fn
+        if total_tpfn > 0:
+            recall = correct_tp/total_tpfn
+        accuracy = (correct_tp + correct_tn)/(total_negative+total_positive)
+      
 
         
         loss_list.append(train_loss + val_loss)
         val_loss_list.append(val_loss)
-        accuracy_0_list.append(accuracy_0)
-        accuracy_1_list.append(accuracy_1)
+        precision_list.append(precision)
+        recall_list.append(recall)
         accuracy_list.append(accuracy)
         train_loss_list.append(train_loss)
 
-        print(f"Epoch {epoch+1}/{epochs}, Train Loss: {train_loss:.4f}, Val Loss: {val_loss:.4f}, Accuracy(0): {accuracy_0:.4f}, Accuracy(1): {accuracy_1:.4f}")
+        print("Epoch {0}/{1}, Train Loss: {2:.4f}, Val Loss: {3:.4f}, Precision: {4:.4f}, Recall: {5:.4f}, Accuracy: {6:.4f}".format(
+                  (epoch+1),epochs,
+                  train_loss,
+                    val_loss,
+                    precision,
+                    recall,
+                    accuracy
+              ))
 
 
     enddtime = time.time()
     print(f"Train time: {enddtime - starttime}")
-    return model, loss_list,train_loss_list,val_loss_list,accuracy_list, accuracy_0_list, accuracy_1_list
+    return model, loss_list,train_loss_list,val_loss_list,precision_list,recall_list, accuracy_list
